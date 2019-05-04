9c41a296719fb579bde5fff110788fe53bf2b8fb
swiftalicious <amazinggrace1@protonmail.com>
2019-05-03 21:56:40 -0700

    Starting project

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieList (IOSPT1)
+//
+//  Created by Jason on 5/3/19.
+//  Copyright © 2019 Jason Thomas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
